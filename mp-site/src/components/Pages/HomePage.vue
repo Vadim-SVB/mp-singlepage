90854dac5766ff0562396b2598e85e23e39419ce
Vadim-SVB <59886821+Vadim-SVB@users.noreply.github.com>
2022-04-27 13:15:24 +0300

    Develop (#10)
    * add options list
    
    * plus options list
    
    * add vuejs site structure
    
    add i18n translations ru en lt
    
    * add vue js

---
+++ mp-site/src/components/Pages/HomePage.vue
@@ -0,0 +1,40 @@
+<template>
+        <section class="relative overflow-hidden h-screen">
+        <img src="img/img_1.jpg" class="absolute h-full w-full object-cover backdrop-filter backdrop-brightness-0"/>
+            <div class="bg-black bg-opacity-50 mx-auto px-6 flex flex-wrap content-center h-full relative py-16">
+                <div class="w-full sm:w-full lg:w-3/5 xl:w-8/12 flex items-center flex-col place-content-center  lg:items-start relative">
+                    <h1 class="font-bebas-neue font-sans tracking-wide uppercase text-xl sm:text-2xl md:text-3xl lg:text-3xl xl:text-4xl 2xl:text-6xl font-black  flex  flex-col leading-none dark:text-white text-gray-200">
+                        {{ $t("main_page.slogan.first") }} 
+                        <span class="tracking-normal text-3xl sm:text-4xl md:text-5xl lg:text-5xl xl:text-6xl 2xl:text-8xl">
+                            {{ $t("main_page.slogan.second") }} 
+                        </span>
+                    </h1>
+                    <p class="font-sans lg:tracking-wider text-base sm:text-xl md:text-2xl lg:text-2xl xl:text-3xl 2xl:text-5xl text-gray-50 dark:text-white">
+                        {{ $t("main_page.slogan.third") }} 
+                    </p>
+                    
+                </div>
+                <div class="w-full sm:w-full lg:w-2/5 xl:w-4/12 flex justify-center">
+                    <button class="px-6 py-3 w-40 mt-8 mb-8 lg:px-12 lg:py-7 lg:w-80 lg:mt-16 lg:mb-16 bg-black bg-opacity-20 font-light transition ease-in duration-200 hover:border-yellow-200 hover:text-yellow-200 border-4 text-xs lg:text-lg text-yellow-400 border-yellow-400  focus:outline-none">
+                        {{ $t("main_page.btn_projects") }}
+                    </button>
+                </div>
+
+                
+                <!-- MOUSE ICON Scroll -->
+                <div class="absolute  bottom-2 lg:bottom-12 left-0 right-0 h-20 text-gray-500 flex flex-col place-content-center place-items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="hidden lg:block h-11 w-11 fill-current" viewBox="0 0 16 16">
+                        <path d="M8 3a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 3zm4 8a4 4 0 0 1-8 0V5a4 4 0 1 1 8 0v6zM8 0a5 5 0 0 0-5 5v6a5 5 0 0 0 10 0V5a5 5 0 0 0-5-5z"/>
+                      </svg>
+                      <svg xmlns="http://www.w3.org/2000/svg"  class=" h-6 w-6 fill-current animate-bounce" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                      </svg>
+                </div>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+  name: "HomePage",
+}
+</script>
---
+++ mp-site/src/components/Pages/PanelPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="bg-black relative overflow-hidden h-screen">
+    <img src="img/img_3.jpg" class="absolute h-full w-full object-cover backdrop-filter backdrop-brightness-0"/>
+        <!-- <img src="./img/panel-naruzh_.png" class="h-3/3 bg-local bg-clip-content"/> -->
+      <article class="h-full  py-0 px-4 flex flex-col lg:flex-row-reverse flex-wrap relative bg-black bg-opacity-50 overflow-y-auto text-sm text-white break-words bg-gradient-to-t from-gray-900 via-transparent to-transparent overflow-hidden">
+            
+        <div class=" h-2/3 w-full lg:w-2/3 lg:h-full  flex flex-col items-start justify-between pt-24 border-l-2 border-yellow-500">
+          <ul class=" py-4   z-10 text-sm sm:text-lg md:text-xl lg:text-2xl">
+            <li class="py-10 px-5 text-sm sm:text-lg md:text-xl lg:text-2xl border-r-2 border-t-2 border-b-2 border-yellow-400 uppercase font-medium text-center bg-black">{{ $t("panel.title") }}</li>
+            <li class="px-2 py-2 ml-5 mt-5 w-full bg-gradient-to-r from-gray-900 bg-opacity-80">{{ $t("panel.plus_0") }} <br> ({{ $t("panel.plus_1") }})</li>
+            <li class="px-2 py-2 ml-5 w-full bg-gradient-to-r from-gray-900 bg-opacity-80">{{ $t("panel.plus_2") }}</li>
+            <li class="px-2 py-2 ml-5 w-full bg-gradient-to-r from-gray-900 bg-opacity-80">{{ $t("panel.plus_3") }}</li>
+            <li class="px-2 py-2 ml-5 w-full bg-gradient-to-r from-gray-900 bg-opacity-80">{{ $t("panel.plus_4") }}</li>
+            <li class="px-2 py-2 ml-5 w-full bg-gradient-to-r from-gray-900 bg-opacity-80">{{ $t("panel.plus_5") }}</li>
+            <li class="px-2 py-2 ml-5 w-full bg-gradient-to-r from-gray-900 bg-opacity-80">{{ $t("panel.plus_6") }}</li>
+          </ul>
+
+          <button class="max-w-min px-5 py-3 mb-20 z-10  uppercase text-lg md:text-2xl hidden lg:block transition ease-in duration-200 hover:border-yellow-200 hover:text-yellow-200 border-2 border-yellow-400 text-yellow-400 bg-black bg-opacity-80">{{ $t("panel.btn_panel_info") }}</button>
+            </div>
+            <!-- PANEL IMG -->
+            <div class="h-1/3 w-full lg:w-1/3 lg:h-full lg:border-r-2 lg:border-b-8 lg:border-gray-400 flex items-center justify-center ">
+                <img class="absolute bottom-0 -left-5" src="img/panel_sectional_view/@2x.png" alt="">
+                <button class="max-w-min px-5 py-3 z-10 uppercase text-lg md:text-2xl lg:hidden transition ease-in duration-200 hover:border-yellow-200 hover:text-yellow-200 border-2 border-yellow-400 text-yellow-400 bg-black bg-opacity-80">{{ $t("panel.btn_panel_info") }}</button>
+            </div>
+        </article>
+        
+    </section>
+</template>
+<script>
+export default {
+  name: "PanelPage",
+  data(){},
+  methods: {},
+};
+</script>
